7e60d7f44571aad4ee5db96661a8a525b6b49ba2
Ismael Contreras <smartdom.biz@gmail.com>
2022-09-30 15:14:11 +0200

    minors updates

--- app/Helpers/ProductHelper.php
+++ app/Helpers/ProductHelper.php
@@ -5,17 +5,20 @@ use Netflie\WhatsAppCloudApi\Message\Media\LinkID;
 use Netflie\WhatsAppCloudApi\WhatsAppCloudApi;
 
 function sendWSCatalogue($phone, $path){
-    $phone=preg_replace('/[^0-9]/', '', $phone);
-   
+    $user=auth()->user();
+    $phone='1'.preg_replace('/[^0-9]/', '', $phone);
+    $phone2='1'.preg_replace('/[^0-9]/', '', $user->phone);
     $whatsapp_cloud_api = new WhatsAppCloudApi([
         'from_phone_number_id' => env('WHATSAPP_NUMBER_ID'),
         'access_token' => env('WHATSAPP_TOKEN'),
     ]);
     $document_name = basename($path);
-    $document_caption = 'Catálogo de productos';
-
+    $document_caption =  'Catálogo de productos';
+    $user=auth()->user();
     $document_link = $path;
     $link_id = new LinkID($document_link);
-    $whatsapp_cloud_api->sendDocument('1'.$phone, $link_id, $document_name, $document_caption);
-    $whatsapp_cloud_api->sendTextMessage('1'.$phone, 'Verifica nuestro '.$document_caption);
+    $whatsapp_cloud_api->sendDocument($phone, $link_id, $document_name, $document_caption);
+    $whatsapp_cloud_api->sendDocument($phone2, $link_id, $document_name, $document_caption);
+    $whatsapp_cloud_api->sendTextMessage($phone, 'Verifica nuestro '.$document_caption);
+    $whatsapp_cloud_api->sendTextMessage($phone2, 'Verifica nuestro '.$document_caption);
 }
