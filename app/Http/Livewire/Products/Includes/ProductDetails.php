1796cd4af82db8f0282e6a368cf838a7ad7cffdc
Ismael Contreras <smartdom.biz@gmail.com>
2022-11-10 02:02:52 +0100

    Bugs: set price on 362 on invoice, issues with 606

--- app/Http/Livewire/Products/Includes/ProductDetails.php
+++ app/Http/Livewire/Products/Includes/ProductDetails.php
@@ -4,7 +4,11 @@ namespace App\Http\Livewire\Products\Includes;
 
 use App\Http\Livewire\UniqueDateTrait;
 use Carbon\Carbon;
-use App\Http\Classes\Column;use Mediconesystems\LivewireDatatables\DateColumn;
+use App\Http\Classes\Column;
+use App\Http\Classes\NumberColumn as ClassesNumberColumn;
+use App\Models\Detail;
+use Illuminate\Support\Facades\DB;
+use Mediconesystems\LivewireDatatables\DateColumn;
 use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
 use Mediconesystems\LivewireDatatables\NumberColumn;
 
@@ -18,28 +22,25 @@ class ProductDetails extends LivewireDatatable
     public function builder()
     {
         
-         return $this->product->details()->with('unit','detailable.client');
+        $details=Detail::where('product_id',$this->product->id)
+        ->join('units','details.unit_id','units.id')
+        ->join('invoices','details.detailable_id','invoices.id')
+        ->where('detailable_type','App\Models\Invoice');
+        return $details;
     }
 
     public function columns()
     {
-        $details=$this->builder()->get()->toArray();
         return [
-            DateColumn::name('created_at')->label('Fecha')->format('d/m/Y h:i A')->filterable(),
-            Column::callback('cant', function($cant){
-                return formatNumber($cant);
-            })->label('Cant.')->enableSummary(),
-            Column::callback(['discount_rate'], function($discount_rate){
-                return formatNumber($discount_rate*100).'%';
-            })->label('Desc.'),
-            Column::callback(['detailable_id','id'], function($det, $id) use ($details){
-                $result=arrayFind($details, 'id',$id);
-                return $result['detailable']['number'];
-            })->label('Fact.'),
-            Column::callback(['total'], function($total){
-                return '$'.formatNumber($total);
-            })->label('Monto')->enableSummary()->contentAlignRight(),
-          
+            DateColumn::name('details.created_at')->label('Fecha')->format('d/m/Y h:i A')->filterable(),
+            ClassesNumberColumn::name('details.cant')->label('Cant')->formatear('number')->enableSummary(),
+            ClassesNumberColumn::name('details.price')->label('Precio')->formatear('money'),
+            ClassesNumberColumn::raw("CONCAT(ROUND(details.discount_rate*100,2),'%') AS rate")->label('Desc.'),
+            ClassesNumberColumn::name('details.total')->label('Total')->formatear('money')->enableSummary(),
+            Column::callback(['invoices.id','invoices.number'], function ($id,$number) {
+                return "<a class='text-blue-500 hover:underline hover:font-bold' href=".route('invoices.show',$id)."> Fact. ".ltrim(substr($number, strpos($number, '-') + 1), '0')."</a>";
+            })->label('Factura'),
+           
         ];
     }
     public function summarize($column)
