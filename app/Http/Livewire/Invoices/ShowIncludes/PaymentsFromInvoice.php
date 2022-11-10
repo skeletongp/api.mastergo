0796113c31c9379e5042e4d665d550a7cdd6b76b
Ismael Contreras <smartdom.biz@gmail.com>
2022-11-10 02:33:36 +0100

    added fuuntion to delete payment

--- app/Http/Livewire/Invoices/ShowIncludes/PaymentsFromInvoice.php
+++ app/Http/Livewire/Invoices/ShowIncludes/PaymentsFromInvoice.php
@@ -3,22 +3,30 @@
 namespace App\Http\Livewire\Invoices\ShowIncludes;
 
 use Mediconesystems\LivewireDatatables\Action;
-use App\Http\Classes\Column;use Mediconesystems\LivewireDatatables\DateColumn;
+use App\Http\Classes\Column;
+use App\Http\Classes\NumberColumn;
+use App\Models\Payment;
+use Mediconesystems\LivewireDatatables\DateColumn;
 use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
 
 class PaymentsFromInvoice extends LivewireDatatable
 {
     public $invoice;
-    public $hideable="select";
-    public $headTitle='Historial de pagos';
+    public $hideable = "select";
+    public $headTitle = 'Historial de pagos';
     public function builder()
     {
-        return $this->invoice->payments()->with('payable.store', 'payable.payment', 'payer', 'payer', 'place.preference', 'contable')->orderBy('id','desc');
+        $payments = Payment::where('payable_id', $this->invoice->id)->where('payable_type', 'App\Models\Invoice')
+            ->join('moso_master.users', 'users.id', '=', 'payments.contable_id')
+            ->select('payments.*', 'users.fullname as cajero')
+            ->orderBy('payments.id', 'desc');
+
+        return $payments;
     }
 
     public function columns()
     {
-        
+
         return [
             DateColumn::name('updated_at')->label('Fecha')->hide()->format('d/m/Y H:i A'),
             Column::name('total')->callback(['total'], function ($total) {
@@ -32,27 +40,54 @@ class PaymentsFromInvoice extends LivewireDatatable
             })->label('Transf.'),
             Column::name('tarjeta')->callback(['tarjeta'], function ($tarjeta) {
                 return '$' . formatNumber($tarjeta);
-            })->label('Otros'),
+            })->label('Otros')->hide(),
 
             Column::name('payed')->callback(['payed'], function ($payed) {
-                return "<b>$". formatNumber($payed)."</b>";
+                return "<b>$" . formatNumber($payed) . "</b>";
             })->label('Pagado'),
-            Column::callback(['rest'], function ($rest) {
-                return  "<span class='text-red-400 font-bold'>$". formatNumber($rest)."</span>";
-            })->label('Resta'),
-            Column::callback(['cambio'], function ($cambio) {
-                return  "<span class='text-blue-600 font-bold'>$". formatNumber($cambio)."</span>";
-            })->label('Cambio'),
-            Column::callback(['payer_id', 'id'], function ($payer, $id)  {
+            NumberColumn::name('payments.rest')->label('Resta')->formatear('money', 'text-red-400')->hide(),
+            NumberColumn::name('payments.cambio')->label('Cambio')->formatear('money', 'text-blue-600 font-bold'),
+
+            Column::callback(['payer_id', 'id'], function ($payer, $id) {
                 return  "<span class='far fa-print cursor-pointer' wire:click='print($id)'> </span>";
             })->label('Print')->contentAlignCenter(),
+            Column::delete()->label('Del.')
         ];
     }
     public function print($id)
     {
-        $payments=$this->builder()->get()->toArray();
-        $result=arrayFind($payments, 'id', $id);
+        $payments = $this->builder()->get()->toArray();
+        $result = arrayFind($payments, 'id', $id);
         $this->emit('printPayment', $result);
     }
-    
+    public function delete($id)
+    {
+        $user = auth()->user();
+        if ($user->hasRole(['Super Admin', 'Admin'])) {
+            $place = getPlace();
+            $payment = Payment::with('payable.client', 'payer')->whereId($id)->first();
+            $firstPayment = Payment::where('payable_id', $payment->payable_id)->where('payable_type', $payment->payable_type)->orderBy('id', 'asc')->first();
+            if ($firstPayment->id == $payment->id) {
+                $this->emit('showAlert', 'No se puede eliminar el primer pago de la factura','error');
+                return;
+            }
+            $this->backToInvoice($payment->payable, $payment->payed - $payment->cambio);
+            setTransaction('Reversión pago de ' . $payment->payer->name, $payment->payable->number, $payment->payed - $payment->cambio, $payment->payer->contable, $place->cash());
+            $payment->delete();
+            $this->emit('showAlert', 'El pago ha sido eliminado','success');
+        } else {
+            $this->emit('showAlert', 'No puede eliminar pagos', 'error');
+        }
+        
+    }
+    public function backToInvoice($invoice, $payed)
+    {
+        $invoice->update([
+            'rest' => $invoice->rest + $payed,
+        ]);
+        $invoice->client->update([
+            'debt' => $invoice->client->invoices->sum('rest'),
+            'limit' => $invoice->client->limit + $payed
+        ]);
+    }
 }
